9c18beeac7c6063a465b694326007acb54388aec
crist <raducristina18@stud.ase.ro>
2024-05-12 11:56:10 +0300

    delete @ExceptionHandler for Exception.class

--- src/main/java/storemanagementtool/store/exception/GlobalExceptionHandler.java
+++ src/main/java/storemanagementtool/store/exception/GlobalExceptionHandler.java
@@ -20,12 +20,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGeneralException(Exception ex) {
-        logger.error("An unexpected error occurred: " + ex.getMessage(), ex);
-        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
     @ExceptionHandler(ProductOutOfStockException.class)
     public ResponseEntity<String> handleProductOutOfStockException(ProductOutOfStockException ex) {
         logger.error("Error: " + ex.getMessage());
